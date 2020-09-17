d99ba900f550ff4d57eed1fcd41f4e901aded013
MrHencke <hencke1@hotmail.com>
2020-09-17 02:27:11 +0200

    first commit :zap:

---
+++ README.md
@@ -0,0 +1,5 @@
+# Electron-TicTacToe
+
+A simple tictactoe game written in html, css and js, originally written for electron.
+
+My goal with porting this to a pure web version is trying out github pages.
---
+++ scripts/gamestate.js
@@ -0,0 +1,18 @@
+const gamelogic = require("./gamelogic.js");
+
+let gameState;
+var player1;
+var player2;
+let currentPlayer;
+let count = 0;
+
+gamelogic.initializeGame(); //method call works, checked with consolelog
+
+document.querySelectorAll(".buttonbox").forEach((cell) => {
+	cell.addEventListener("click", function () {
+		gamelogic.setTile(cell.id);
+		gamelogic.winCheck(gameState);
+		console.log(gameState);
+		gamelogic.restart(gameState);
+	});
+});
